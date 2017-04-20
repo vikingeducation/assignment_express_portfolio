672bd2af2238f0fae46bd9400f59ebcc95f86648
Christian Florez <florez.christian@gmail.com>
2017-04-20 17:01:55 -0400

    finish implementation, begin styles

---
+++ routes/projects.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const {Project, projects} = require('../models/projects');
+
+const findProject = (id) => {
+  return projects.find(project => id == project.id);
+};
+
+router.get('/:id', (req, res, next) => {
+  const project = findProject(req.params.id);
+  res.render('project', {project: project});
+});
+
+module.exports = router;
