a6b0e0bf906551282c1671ef551ac653313721ba
Tyler Ketron <tketron@gmail.com>
2017-04-30 13:01:49 -0700

    implemented project iteration and routing for index page

--- portfolio/models/projects.js
+++ portfolio/models/projects.js
@@ -12,7 +12,7 @@ const projects = [
   new Project("Facebook Clone", "https://github.com/tketron/project_prep_facebook_pages", "../public/images/facebook-about.png", "HTML", "CSS", "SASS"),
   new Project("jQuery Widgets", "https://github.com/tketron/assignment_jq_ee_sprint", "../public/images/jquery.png", "jQuery", "Javascript", "HTML", "CSS"),
   new Project("Node Server", "https://github.com/tketron/assignment_build_a_nodejs_server", "../public/images/node.png", "Node.js", "Javascript", "HTML")
-]
+];
 
 module.exports = {
   Project,
