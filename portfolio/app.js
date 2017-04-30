c50f476d61a6f2075fd428294356d87d461ba820
Tyler Ketron <tketron@gmail.com>
2017-04-30 13:43:08 -0700

    added styling to reduce image size

--- portfolio/app.js
+++ portfolio/app.js
@@ -26,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/projects', projects);
-// app.use(express.static(path.join(__dirname,'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
