b718b7f9514b54c719280c953036c484eb73f806
anne <richardson.ae@gmail.com>
2017-08-09 15:51:35 -0500

    Add placeholder images to project data

--- models/projects.js
+++ models/projects.js
@@ -8,11 +8,11 @@ class Project {
 }
 
 const projects = [
-  new Project("Odin and Friends", "https://github.com/lortza/odin_and_friends", "image.jpg", 'javascript', 'node', 'handlebars', 'express'),
-  new Project('Express Hello World', 'https://github.com/lortza/express_hello', 'image.jpg', 'javascript', 'node', 'handlebars', 'express', 'heroku'),
-  new Project('First Node Server', 'https://github.com/lortza/assignment_build_a_nodejs_server', 'image.jpg', 'javascript', 'node'),
-  new Project('Royalty Free Music Player', 'https://github.com/lortza/assignment_royalty_free_music_player', 'image.jpg', 'jQuery', 'javascript', 'html', 'css', 'sass'),
-  new Project('To Do List Lite', 'https://github.com/lortza/todolistlite', 'image.jpg', 'html', 'css', 'jQuery', 'javascript')
+  new Project("Odin and Friends", "https://github.com/lortza/odin_and_friends", "http://via.placeholder.com/200x200", 'javascript', 'node', 'handlebars', 'express'),
+  new Project('Express Hello World', 'https://github.com/lortza/express_hello', 'http://via.placeholder.com/200x200', 'javascript', 'node', 'handlebars', 'express', 'heroku'),
+  new Project('First Node Server', 'https://github.com/lortza/assignment_build_a_nodejs_server', 'http://via.placeholder.com/200x200', 'javascript', 'node'),
+  new Project('Royalty Free Music Player', 'https://github.com/lortza/assignment_royalty_free_music_player', 'http://via.placeholder.com/200x200', 'jQuery', 'javascript', 'html', 'css', 'sass'),
+  new Project('To Do List Lite', 'https://github.com/lortza/todolistlite', 'http://via.placeholder.com/200x200', 'html', 'css', 'jQuery', 'javascript')
 ];
 
 module.exports = {
