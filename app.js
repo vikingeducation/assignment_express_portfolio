0092abfa79ca106d074ce175a5e25ac83d2ebbcf
Jared Gebel <jaredjgebel@gmail.com>
2017-09-21 21:06:30 -0400

    Complete portfolio page structure and content

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var project = require('./routes/project');
 
 var app = express();
 
@@ -21,9 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static('public'))
 
+// Routes
 app.use('/', index);
 app.use('/users', users);
+app.use('/projects', project)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/projects.js
+++ models/projects.js
@@ -6,15 +6,17 @@ class Project {
     this.repoSrc = repoSrc;
     this.lang = lang;
     this.description = description;
+    this.path = "/projects/" + encodeURIComponent(name);
   }
 }
 
-// Instantiate each project in an array
+// Instantiate each project in an arrays
+// Files are referenced from root of app (app.js)
 const projects = [
-  new Project("Royalty-Free Music Player", "../public/player.JPG", "https://github.com/jaredjgebel/assignment_royalty_free_music_player", "HTML, CSS, SASS, Javascript, jQuery", "Simple music player in the browser"),
-  new Project("Sample Bootstrap Page", "../public/images/bootstrap.JPG", "https://github.com/jaredjgebel/assignment_bootstrap_sprint", "HTML, CSS, SASS, Bootstrap", "Recreate a sample bootstrap page with four breakpoints"),
-  new Project("Facebook Lookalike", "../public/images/facebook.JPG", "https://github.com/jaredjgebel/project_prep_facebook_pages", "HTML, CSS, SASS, 960 Grid Template", "Recreate Facebook homepage, profile page, and friends page"),
-  new Project("CSS Garden", "../public/images/garden.JPG", "https://github.com/jaredjgebel/assignment_viking_css_garden", "HTML, CSS")
+  new Project("Royalty-Free Music Player", "/images/player.JPG", "https://github.com/jaredjgebel/assignment_royalty_free_music_player", "HTML, CSS, SASS, Javascript, jQuery, Git", "Simple music player in the browser"),
+  new Project("Sample Bootstrap Page", "/images/bootstrap.JPG", "https://github.com/jaredjgebel/assignment_bootstrap_sprint", "HTML, CSS, SASS, Bootstrap, Git", "Recreate a sample bootstrap page with four breakpoints"),
+  new Project("Facebook Lookalike", "/images/facebook.JPG", "https://github.com/jaredjgebel/project_prep_facebook_pages", "HTML, CSS, SASS, 960 Grid Template, Git", "Recreate Facebook homepage, profile page, and friends page"),
+  new Project("CSS Garden", "/images/garden.JPG", "https://github.com/jaredjgebel/assignment_viking_css_garden", "HTML, CSS, Git", "Styled a given HTML template with straight CSS")
 ];
 
 module.exports = {
