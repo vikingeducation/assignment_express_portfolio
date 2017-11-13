e5b08b8977e81e0a928a2cebb9d3c4c6528d0b8d
Johann <baptistajohann@gmail.com>
2017-11-12 22:41:20 -0800

    Completed basics

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const {Project, projects} = require('../models/projects');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { projects : projects });
+});
+
+module.exports = router;
