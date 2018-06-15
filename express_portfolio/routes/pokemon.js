b8eabf62fda238fba986308829f18049b1718a5d
Megan Taylor <mtaylor@megantaylor.org>
2018-06-15 17:24:08 -0400

    more routes

--- express_portfolio/routes/pokemon.js
+++ express_portfolio/routes/pokemon.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var pokemon = require('../models/pokemon').pokemon;
+
+function findPokemon ( param ) {
+  return pokemon.find( pokemon => {
+    return param == pokemon.param
+  })
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:pokemon', function(req, res, next) {
+  // res.send('respond with a resource');
+  var pokemon = findPokemon(req.params.pokemon)
+  +  res.render('pokemon', { pokemon: pokemon })
 });
 
 module.exports = router;
