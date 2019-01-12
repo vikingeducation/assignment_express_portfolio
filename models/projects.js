12e8cc2f368591313289101486086711bd999e9c
xmodmap <josephkottenstette@gmail.com>
2019-01-11 19:14:46 -0800

    begin individual projct show pages

---
+++ models/projects.js
@@ -0,0 +1,13 @@
+function Project(name, techs, link, img){
+    this.name = name;
+    this.techs = techs;
+    this.link = link;
+    this.img = img;
+};
+
+const projects = [
+  new Project("Project 1", ["javascript", "Express"], "www.asdf.com", "http://www.clker.com/cliparts/3/m/v/Y/E/V/small-red-apple-md.png"),
+  new Project("Project 2", ["javascript2", "Express2"], "www.asdf22.com", "http://www.clker.com/cliparts/3/m/v/Y/E/V/small-red-apple-md.png")
+];
+
+module.exports = {Project, projects};
